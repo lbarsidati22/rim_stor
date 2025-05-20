b91b7e40dd4d8d9e6d7f634bf8ce9cccf655d4f1
lbarsidati22 <lbarsidati575@gmail.com>
2025-05-20 18:51:00 +0000

    screen utils done

--- lib/rim_stor_app.dart
+++ lib/rim_stor_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rim_stor/core/app/connectivity_controller.dart';
 import 'package:rim_stor/core/common/screens/no_network_screen.dart';
 
@@ -11,19 +12,23 @@ class RimStorApp extends StatelessWidget {
       valueListenable: ConnectivityController.inistanc.isConnected,
       builder: (_, value, widget) {
         if (value) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(builder: (context) {
-                  ConnectivityController.inistanc.init();
-                  return widget!;
-                }),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text('Rim Stor'),
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(builder: (context) {
+                    ConnectivityController.inistanc.init();
+                    return widget!;
+                  }),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Rim Stor'),
+                ),
               ),
             ),
           );

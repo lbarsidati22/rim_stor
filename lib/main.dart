ed5b4a1b119e0430ec0edeeccfcefe37d776cc58
lbarsidati22 <lbarsidati575@gmail.com>
2025-05-23 00:24:49 +0000

    add more  packages freezed && bloc %% shared prefrences

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rim_stor/core/app/bloc_observer.dart';
 import 'package:rim_stor/core/app/env.variables.dart';
 import 'package:rim_stor/rim_stor_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
   await Firebase.initializeApp();
   await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   await SystemChrome.setPreferredOrientations([

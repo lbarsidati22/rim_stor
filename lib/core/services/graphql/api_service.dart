80755cda90e9ae3321147222f74e54c0cdf6fa4c
lbarsidati22 <lbarsidati575@gmail.com>
2025-06-03 22:10:22 +0000

    errors

--- lib/core/services/graphql/api_service.dart
+++ lib/core/services/graphql/api_service.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:rim_stor/features/auth/data/login_request_body.dart';
+import 'package:rim_stor/features/auth/data/login_response.dart';
 
 part 'api_service.g.dart';
 
@@ -9,4 +11,12 @@ const String graphql = '/graphql';
 @RestApi(baseUrl: baseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+  @POST(graphql)
+  Future<LoginResponse> login(
+    @Body() Map<String, dynamic> mutasion,
+  );
+  @GET('/api/v1/auth/profile')
+  Future<LoginResponse> userRole(
+    @Body() Map<String, dynamic> getUserRole,
+  );
 }
